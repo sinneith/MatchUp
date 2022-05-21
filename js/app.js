57c7e319e33939c41676e3b433583ff64db7cd15
sinsin <wr1126@naver.com>
2022-05-22 05:49:34 +0900

    add check card match function

--- js/app.js
+++ js/app.js
@@ -3,7 +3,30 @@ const mediumBtn = document.getElementById("medium");
 const hardBtn = document.getElementById("hard");
 const cardSlot = document.getElementById("cardSlot");
 
-let cardImg = [];
+let cardImg = [
+  "url(../img/1.png)",
+  "url(../img/2.png)",
+  "url(../img/3.png)",
+  "url(../img/4.png)",
+  "url(../img/5.png)",
+  "url(../img/6.png)",
+  "url(../img/7.png)",
+  "url(../img/8.png)",
+  "url(../img/9.png)",
+  "url(../img/10.png)",
+  "url(../img/11.png)",
+  "url(../img/12.png)",
+  "url(../img/13.png)",
+  "url(../img/14.png)",
+  "url(../img/15.png)",
+  "url(../img/16.png)",
+  "url(../img/17.png)",
+  "url(../img/18.png)",
+];
+let cardArr = []; //카드 div 자체가 담김 with id
+let cardImgArr = []; //랜덤하게 배열된 카드 이미지가 담김
+let isFlipped;
+let flippedCard = {};
 
 function showCard(event) {
   const difficulty = document.getElementById("difficulty");
@@ -18,7 +41,7 @@ function showCard(event) {
       cardNum = 16;
       break;
     case "medium":
-      cardNum = 25;
+      cardNum = 24;
       break;
     case "hard":
       cardNum = 36;
@@ -29,26 +52,77 @@ function showCard(event) {
     card.classList.add("card");
     cardSlot.appendChild(card);
 
+    card.id = i;
+    cardArr.push(card);
+
     switch (cardNum) {
       case 16:
         cardSlot.classList.add("easyWrap");
         card.classList.add("easy");
+        cardImgArr = [...cardImg.slice(0, 8), ...cardImg.slice(0, 8)];
         break;
-      case 25:
+      case 24:
         cardSlot.classList.add("mediumWrap");
         card.classList.add("medium");
+        cardImgArr = [...cardImg.slice(0, 12), ...cardImg.slice(0, 12)];
         break;
       case 36:
         cardSlot.classList.add("hardWrap");
         card.classList.add("hard");
+        cardImgArr = [...cardImg.slice(0, 18), ...cardImg.slice(0, 18)];
         break;
     }
+
+    card.addEventListener("click", flipCard);
   }
-  addCardImg(mode);
+  shuffleCard(cardImgArr);
 }
 
-function addCardImg(mode) {
-  console.log(mode);
+function shuffleCard(arr) {
+  arr.sort(() => Math.random() - 0.5);
+}
+
+function flipCard(event) {
+  const clickedCard = event.target;
+  const index = event.target.id;
+
+  if (clickedCard.classList.contains("correct")) {
+    console.log("booyah");
+  } else {
+    clickedCard.style.backgroundImage = cardImgArr[index];
+
+    if (isFlipped !== true) {
+      flippedCard = {
+        img: clickedCard.style.backgroundImage,
+        index: index,
+      };
+      isFlipped = true;
+    } else {
+      checkMatch(clickedCard, index);
+      isFlipped = false;
+    }
+  }
+}
+
+function checkMatch(clickedCard) {
+  setTimeout(() => {
+    if (flippedCard.img !== clickedCard.style.backgroundImage) {
+      for (let i = 0; i < cardArr.length; i++) {
+        if (cardArr[i].id === flippedCard.index) {
+          cardArr[i].style.backgroundImage = "url(../img/card.png)";
+        }
+      }
+      flippedCard = { img: "", index: "" };
+      clickedCard.style.backgroundImage = "url(../img/card.png)";
+    } else {
+      clickedCard.classList.add("correct");
+      for (let j = 0; j < cardArr.length; j++) {
+        if (flippedCard.index === cardArr[j].id) {
+          cardArr[j].classList.add("correct");
+        }
+      }
+    }
+  }, 700);
 }
 
 easyBtn.addEventListener("click", showCard);
